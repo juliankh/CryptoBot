1684eb6e1653886c15cbf8f63cae3a9060ba03fb
eng1 <yulian.khandros@gmail.com>
2023-05-14 22:11:25 -0400

    using redis

---
+++ src/main/java/com/cb/admin/archived/DataCleaner.java
@@ -0,0 +1,46 @@
+package com.cb.admin.archived;
+
+import com.cb.common.util.NumberUtils;
+import com.cb.db.DbReadOnlyProvider;
+import com.cb.db.DbWriteProvider;
+import com.cb.model.config.archived.DataCleanerConfig;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Singleton
+public class DataCleaner {
+
+    @Inject
+    private DbReadOnlyProvider dbReadOnlyProvider;
+
+    @Inject
+    private DbWriteProvider dbWriteProvider;
+
+    public void prune() {
+        List<DataCleanerConfig> configs = dbReadOnlyProvider.retrieveDataCleanerConfig();
+        log.info("Configs:\n\t" + configs.parallelStream().map(Object::toString).sorted().collect(Collectors.joining("\n\t")));
+        configs.parallelStream().forEach(config -> {
+            String table = config.getTableName();
+            String column = config.getColumnName();
+            int hoursLimit = config.getHoursBack();
+            pruneTable(table, column, hoursLimit);
+        });
+    }
+
+    public void pruneTable(String table, String column, int hoursLimit) {
+        long rowcount = dbWriteProvider.prune(table, column, hoursLimit);
+        log.info("For table [" + table + "] pruned [" + NumberUtils.numberFormat(rowcount) + "] rows which were > [" + hoursLimit + "] hours old");
+    }
+
+    public void cleanup() {
+        log.info("Cleaning up");
+        dbReadOnlyProvider.cleanup();
+        dbWriteProvider.cleanup();
+    }
+
+}
