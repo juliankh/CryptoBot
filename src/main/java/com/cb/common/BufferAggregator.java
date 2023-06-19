021ba6a68cad06acde55ffec00e063414b6cc34d
eng1 <yulian.khandros@gmail.com>
2023-06-19 15:09:36 -0400

    string builder within the buffer aggregator: setting to new one istead of setting length to 0

--- src/main/java/com/cb/common/BufferAggregator.java
+++ src/main/java/com/cb/common/BufferAggregator.java
@@ -4,13 +4,13 @@ import java.util.function.Consumer;
 
 public class BufferAggregator {
 
-    private final StringBuilder sb = new StringBuilder();
+    private StringBuilder sb = new StringBuilder();
 
     public synchronized void process(CharSequence data, boolean last, Consumer<String> consumer) {
         sb.append(data);
         if (last) {
             consumer.accept(sb.toString());
-            sb.setLength(0);
+            sb = new StringBuilder();
         }
     }
 
